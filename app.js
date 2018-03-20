215aa5064bb902fedf26b50edcb7e361b07da11b
rodolfogordo10 <rodolfomotamartins@gmail.com>
2018-03-20 09:21:08 -0300

    merge

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 const http    = require('http');
 const fs      = require('fs');
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin   = require('firebase-admin');
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
-// const banco   = require('./app/core/banco.js');
 const server  = http.createServer();
-
-admin.initializeApp(functions.config().firebase);
-
-const db = admin.database();
 
 exports.run = function () {
     server.on('request', function(req, res) {
@@ -21,24 +10,7 @@ exports.run = function () {
         if (url === '/') {
 
             res.writeHead(200);
-            return res.end(fs.readFileSync('view/index.html'))
-
-        }
-
-        if (url === '/cadastro') {
-
-            // banco._insert(usuario, res);
-
-            var ref = db.ref('assinantes');
-
-            var cpfRef = ref.child("cpf");
-
-            cpfQuery.orderByKey().on('child_added', function(snap) {
-                console.log(snap.getKey(), snap.val());
-            });
-
-            res.writeHead(200);
-            return res.end('show');
+            return res.end(fs.readFileSync('public/index.html'))
 
         }
 
