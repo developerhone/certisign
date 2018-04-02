823f8f14398828a623f47eac3e92118634e8d0b1
Rodolfo Martins <rodolfomotamartins@gmail.com>
2018-04-02 01:11:51 -0300

    merge

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,46 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const md5 = require('md5');
 
 admin.initializeApp(functions.config().firebase);
 
-const db = admin.firestore();
+const db = admin.database();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.addAssinantes = functions.https.onRequest((request, response) => {
 
-    exports.useMultipleWildcards;
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    response.header("Access-Control-Allow-Methods", "GET, PUT, POST");
 
+    console.log(request.method);
     switch (request.method) {
         case 'GET':
-            var getAssinantes = db.doc('assinantes/aeJbZLBQZsPl3PP5gP9d');
 
-            response.status(200).send({ message: getAssinantes });       
-            break;
-        case 'PUT':
-            response.status(200).send({ message: 'put firebase' }); 
-            break;
+            var getAssinantes = [];
+
+            return db.ref('assinantes').once('value', (snapshot) => {
+                // if data exists
+
+                snapshot.forEach((childSnapshot) => {
+                    var childKey = childSnapshot.key;
+                    var childData = childSnapshot.val();
+                    getAssinantes.push(childData);
+                });
+
+                return response.send(200, { message: getAssinantes });
+            }); 
+
         case 'POST':
-            var setAssinante = db.collection('assinantes').doc('aeJbZLBQZsPl3PP5gP9d').set(request.body);
-            response.status(200).send({ message: setAssinante });
-            break;
+
+            return db.ref('assinantes').child(md5(request.body.email)).set(request.body, (err) => {
+                return response.send(200, { id : md5(request.body.email), message: err ? err : 'Salvo com sucesso.' });
+            });
+
         default:
-            response.status(500).send({ error: 'Nenhum method de post encontrado.' });
-            break;
+            return response.send(404);
     }
 
 });
