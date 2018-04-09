1b34ed890d5a9e0d8d4ba7e4fbb9f31a46eca8d5
rodolfogordo10 <rodolfomotamartins@gmail.com>
2018-04-09 10:04:28 -0300

    merge

--- functions/index.js
+++ functions/index.js
@@ -15,32 +15,44 @@ exports.addAssinantes = functions.https.onRequest((request, response) => {
     response.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
     response.header("Access-Control-Allow-Methods", "GET, PUT, POST");
 
-    console.log(request.method);
-    switch (request.method) {
-        case 'GET':
-
-            var getAssinantes = [];
-
-            return db.ref('assinantes').once('value', (snapshot) => {
-                // if data exists
-
-                snapshot.forEach((childSnapshot) => {
-                    var childKey = childSnapshot.key;
-                    var childData = childSnapshot.val();
-                    getAssinantes.push(childData);
-                });
-
-                return response.send(200, { message: getAssinantes });
-            }); 
-
-        case 'POST':
-
-            return db.ref('assinantes').child(md5(request.body.email)).set(request.body, (err) => {
-                return response.send(200, { id : md5(request.body.email), message: err ? err : 'Salvo com sucesso.' });
-            });
-
-        default:
-            return response.send(404);
-    }
+    return db.ref('assinantes').child(md5(request.body.email)).set(request.body, (err) => {
+        return response.send(200, { id : md5(request.body.email), message: err ? err : 'Salvo com sucesso.' });
+    });
 
 });
+
+exports.getAssinantes = functions.https.onRequest((request, response) => {
+
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    response.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+
+    var getAssinantes = [];
+
+    return db.ref('assinantes').once('value', (snapshot) => {
+        // if data exists
+
+        snapshot.forEach((childSnapshot) => {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
+            getAssinantes.push(childData);
+        });
+
+        return response.send(200, { message: getAssinantes });
+    });
+
+});
+
+
+exports.addDownload = functions.https.onRequest((request, response) => {
+
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    response.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+
+    return db.ref('assinantes').child(md5(request.body.email)).set(request.body, (err) => {
+        return response.send(200, { id : md5(request.body.email), message: err ? err : 'Salvo com sucesso.' });
+    });
+
+});
+
